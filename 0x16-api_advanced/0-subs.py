03b51a74af9b1d85a297e5c8c7601dcfd58d661d
Yahia <yn7599@gmail.com>
2024-04-09 23:07:04 +0200

    final commit for reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,5 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "yahia:intranet.alxswe.com:v2.4.1 (by /u/keroxyz)",
     }
     res = requests.get(url=url.format(sub=subreddit), headers=headers)
+    if res.status_code != 200:
+        return 0
     data = res.json()["data"]
     return data.get("subscribers", 0)
