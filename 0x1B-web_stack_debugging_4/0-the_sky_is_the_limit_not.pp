ab1470164c2e05d093dbcfa761b49f0a8b27ecc7
Yahia <yn7599@gmail.com>
2024-04-18 22:49:43 +0200

    restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # executing a command
 exec { 'increase ULIMIT':
-  command => "sed -i 's/15/1024/g' nginx",
+  command => "sed -i 's/15/1024/g' nginx && service nginx restart",
   cwd     => '/etc/default/',
   path    => ['/usr/bin', '/bin']
 }
